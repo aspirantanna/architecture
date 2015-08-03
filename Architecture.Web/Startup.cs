988ec4e945aadc11ecb160c5100d54c4e2778f22
Annadurai <annadurai.net@live.com>
2015-08-03 21:30:33 +0530

    Initial Commit

---
+++ Architecture.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Architecture.Web.Startup))]
+namespace Architecture.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
