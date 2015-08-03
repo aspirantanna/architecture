0b5371cc074825c9b66467377b9925e020504c1f
Annadurai <annadurai.net@live.com>
2015-08-03 22:08:58 +0530

    Data and Entities included in solution

---
+++ Architecture.Data/ArchitectureEntities.cs
@@ -0,0 +1,18 @@
+﻿using Architecture.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Data
+{
+    public class ArchitectureEntities : IdentityDbContext<ApplicationUser>
+    {
+        public ArchitectureEntities() : base("ArchitectureEntities")
+        {
+                
+        }
+    }
+}
